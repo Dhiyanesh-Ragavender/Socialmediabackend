9c9504170ab3d689aca98b9d89cf17aca580f76f
Dhiyanesh-Ragavender <151903671+Dhiyanesh-Ragavender@users.noreply.github.com>
2024-02-25 17:50:37 +0530

    Create authController.js

---
+++ authController.js
@@ -0,0 +1,13 @@
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  // Implementation for user registration
+};
+
+exports.login = async (req, res) => {
+  // Implementation for user login
+};
+
+exports.logout = async (req, res) => {
+  // Implementation for user logout
+};
